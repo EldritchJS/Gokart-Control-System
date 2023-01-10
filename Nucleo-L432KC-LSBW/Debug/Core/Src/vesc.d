fd95d7ffaa74ea9786a75f6fbe07a70438c3f4f6
Jason <jschlessman@gmail.com>
2023-01-10 15:47:31 -0500

    Removed .gitignore'd files that were being tracked

--- Nucleo-L432KC-LSBW/Debug/Core/Src/vesc.d
+++
@@ -1,8 +0,0 @@
-Core/Src/vesc.o: ../Core/Src/vesc.c ../Core/Inc/endianness.h \
- ../Core/Inc/vesc.h ../Core/Inc/vesc_crc.h ../Core/Inc/utils.h \
- ../Core/Inc/vesc_datatypes.h
-../Core/Inc/endianness.h:
-../Core/Inc/vesc.h:
-../Core/Inc/vesc_crc.h:
-../Core/Inc/utils.h:
-../Core/Inc/vesc_datatypes.h:
