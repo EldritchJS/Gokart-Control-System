90ccc5ad3e5ad08aa84de641ba98c7801d284ae2
EldritchJS <jschlessman@gmail.com>
2023-01-19 16:50:39 -0500

    Added console command struct array

--- Nucleo-L432KC-IMU/Application/Src/console.c
+++ Nucleo-L432KC-IMU/Application/Src/console.c
@@ -22,6 +22,26 @@ static osStaticTimerDef_t consoleTimerCB;
 
 osTimerStaticDef(ConsoleTimer, consoleTimerCallback, &consoleTimerCB);
 
+void beginStreaming(void);
+void endStreaming(void);
+void printVersion(void);
+static void printMenu(void);
+
+typedef struct
+{
+  void    (*cmd)(void);
+  char*   cmdName;
+  char*   cmdInfo;
+} CONSOLE_MENU_CMDS;
+
+static CONSOLE_MENU_CMDS menuCmds[] = {
+		{beginStreaming, "B", "Begin streaming if not already started"},
+		{endStreaming, "E", "End streaming if running"},
+		{printVersion, "V", "Print version info"},
+};
+
+static uint32_t numCmds;
+
 static void consoleTimerCallback(void const *arg)
 {
   (void) arg;
@@ -29,47 +49,69 @@ static void consoleTimerCallback(void const *arg)
   osMessagePut(MEMSTaskRXEventQueue, 0, 0);
 }
 
+void beginStreaming(void)
+{
+  if(streamActiveFlag==0)
+  {
+    streamActiveFlag=1;
+	sprintf(outBuffer, "%s\r\n", HEADER_STR);
+	USART1TxStr(outBuffer);
+	osTimerStart(consoleTimer, 10);
+  }
+}
+
+void endStreaming(void)
+{
+  if(streamActiveFlag==1)
+  {
+    streamActiveFlag=0;
+	osTimerStop(consoleTimer);
+  }
+}
+
+void printVersion(void)
+{
+  if(streamActiveFlag==0)
+  {
+    sprintf(outBuffer, "Version: %s\r\n", VERSION_STR);
+    USART1TxStr(outBuffer);
+  }
+}
+
+static void printMenu(void)
+{
+  if(streamActiveFlag==0)
+  {
+	for(int i=0; i<numCmds; i++)
+	{
+	  sprintf(outBuffer, "    %-26s - %s\r\n",menuCmds[i].cmdName, menuCmds[i].cmdInfo);
+	  USART1TxStr(outBuffer);
+	}
+    USART1TxStr("    '?' <ENTER> for help\r\n");
+  }
+}
+
 static void processLine(void)
 {
-  switch(lineBuffer[0])
+  int i;
+  for(i=0; i<numCmds; i++)
   {
-    case 'B':
-    	if(streamActiveFlag==0)
-    	{
-    	  streamActiveFlag=1;
-    	  sprintf(outBuffer, "%s\r\n", HEADER_STR);
-    	  USART1TxStr(outBuffer);
-    	  osTimerStart(consoleTimer, 10);
-    	}
-	    break;
-    case 'E':
-    	if(streamActiveFlag==1)
-    	{
-      	  streamActiveFlag=0;
-    	  osTimerStop(consoleTimer);
-    	}
-    	break;
-    case 'V':
-    	if(streamActiveFlag==0)
-    	{
-    	  sprintf(outBuffer, "Version: %s\r\n", VERSION_STR);
-          USART1TxStr(outBuffer);
-    	}
-    	break;
-    case '?':
-    	if(streamActiveFlag==0)
-    	{
-    	  sprintf(outBuffer, "Menu\r\nB - Begin Streaming\r\nE - End Streaming\r\nV - Print Version\r\n? - This menu\r\n");
-          USART1TxStr(outBuffer);
-    	}
-    	break;
-    default:
-    	if(streamActiveFlag==0)
-    	{
-    	  sprintf(outBuffer, "Unknown command: %c",lineBuffer[0]);
-          USART1TxStr(outBuffer);
-    	}
-    	break;
+    if(lineBuffer[0]==menuCmds[i].cmdName[0])
+    {
+      menuCmds[i].cmd();
+      break;
+    }
+    else if(lineBuffer[i] == '?')
+    {
+      printMenu();
+      break;
+    }
+  }
+
+  if(i==numCmds)
+  {
+	sprintf(outBuffer, "Unknown command: <%c>\r\n", lineBuffer[0]);
+	USART1TxStr(outBuffer);
   }
 }
 
@@ -77,6 +119,7 @@ void console(void)
 {
   uint8_t data;
 
+  numCmds = sizeof(menuCmds)/sizeof(CONSOLE_MENU_CMDS);
   consoleTimer = osTimerCreate(osTimer(ConsoleTimer), osTimerPeriodic, NULL);
   IMUInit();
   for(;;)
@@ -96,6 +139,5 @@ void console(void)
         lineIndex++;
       }
     }
-    osDelay(1);
   }
 }
